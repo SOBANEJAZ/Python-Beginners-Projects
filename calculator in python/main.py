30833e66a2318503f3ba776982951df0fe57448b
Soban <sobanpythonista@gmail.com>
2023-06-19 20:43:50 +0500

    initial commit
    I have added some of my projects

---
+++ calculator in python/main.py
@@ -0,0 +1,27 @@
+# BAsic calculator in python
+while True:
+
+    operator = input("Enter operator(+ - * /): \n \t ")
+    num1 = float(input("Enter your first number:"))
+    num2 = float(input("Enter your second number:"))
+
+    if operator == "+":
+        result = num1 + num2
+        print(result)
+    elif operator == "-":
+        result = num1 - num2
+        print(result)
+    elif operator == "*":
+        result = num1 * num2
+        print(result)
+    elif operator == "/":
+        result = num1 / num2
+        print(result)
+    else:
+        print("Invalid operator")
+
+    p = input("Do you want to continue(y/n): ")
+    if p == "y":
+        continue
+    elif p == "n":
+        break
