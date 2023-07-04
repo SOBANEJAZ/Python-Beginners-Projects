1b6444a92f6e78aabf8d7b401476a90a270f3c07
Soban <sobanpythonista@gmail.com>
2023-07-04 15:15:03 +0500

    added head and tails

---
+++ 1. Python Starter Projects/head and tail/main.py
@@ -0,0 +1,26 @@
+""" Heads & Tails Cricket Game
+ Two modes:
+            1. Computer Mode
+            2. Two Players Mode"""
+import random
+
+runs = [1, 2, 3, 4, 5, 6]
+
+print('\n Welcome to the "Heads & Tails Cricket Game" \n Please select a mode:')
+mode_select = int(input('"computer"(1) or "two players"(1):\n(1/2):'))
+
+
+def comp_mode():
+    global runs
+    player_toss = input("heads or tails:")
+    player_num = int(input(f"please choose a number from \"{runs}:"))
+    if player_toss ==
+            
+
+if mode_select == 1:
+    comp_mode()
+elif mode_select == 2:
+    pass
+elif mode_select != 1 and 2: # Fun Fact I hate else
+    print("Please Enter \"1\" or \"2\" to select mode")
+
