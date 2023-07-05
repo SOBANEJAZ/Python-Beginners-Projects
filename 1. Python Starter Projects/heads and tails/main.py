c3c7db607871e2e0162adac317dd777ea3fefc73
Soban <sobanpythonista@gmail.com>
2023-07-05 19:26:40 +0500

    Completed the program

--- 1. Python Starter Projects/heads and tails/main.py
+++ 1. Python Starter Projects/heads and tails/main.py
@@ -1,26 +1,18 @@
-""" Heads & Tails Cricket Game
- Two modes:
-            1. Computer Mode
-            2. Two Players Mode"""
-import random
+# Heads and Tails stimulator
 
-runs = [1, 2, 3, 4, 5, 6]
+import random  # Random makes it easy to choose in these situations
 
-print('\n Welcome to the "Heads & Tails Cricket Game" \n Please select a mode:')
-mode_select = int(input('"computer"(1) or "two players"(1):\n(1/2):'))
+print('.\n \tWelcome to the "Heads & Tails Cricket Game"\n')  # Greetings
 
 
-def comp_mode():
-    global runs
-    player_toss = input("heads or tails:")
-    player_num = int(input(f"please choose a number from \"{runs}:"))
-    if player_toss ==
-            
+choices = [["heads", "head"], ["tails", "tail"]]  # Some people forget the 's' offtenly
+choice_player = input("\tSelect 'Heads' or 'Tails':\n\t\t").lower()
+toss = random.choice(choices)
 
-if mode_select == 1:
-    comp_mode()
-elif mode_select == 2:
-    pass
-elif mode_select != 1 and 2: # Fun Fact I hate else
-    print("Please Enter \"1\" or \"2\" to select mode")
-
+if choice_player in choices[0] or choice_player in choices[1]:
+    if choice_player in toss:
+        print("You have won!!!")
+    else:
+        print("You lost!!!")
+else:
+    print("Please input either 'Heads' or 'Tails'")
