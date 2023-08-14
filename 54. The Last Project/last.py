6571388c81c1b1e5c9ad1c43ec3591a97a10cfad
Soban <sobanpythonista@gmail.com>
2023-08-14 17:57:43 +0500

    added last program

---
+++ 54. The Last Project/last.py
@@ -0,0 +1,5 @@
+print("""It was a good experience doing these projects i will not be adding any projects into this repo because
+       I will focus more on the Python 2D game repo and more on machine leaning
+      """)
+
+print("Thank you very much if you checked out the repository")
