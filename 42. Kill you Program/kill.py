4facb864aa475da8eeec8e4991e7356bbc5d6420
Soban <sobanpythonista@gmail.com>
2023-08-14 16:34:45 +0500

    Create kill.py

---
+++ 42. Kill you Program/kill.py
@@ -0,0 +1,3 @@
+# This program does nothing illegal sorry 
+
+print("I wanted to tell you that there are other projects that are better than this and it was just a clickbait. Go run the tkinter projects.")
