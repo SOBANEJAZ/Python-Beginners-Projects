2b8ca067ae0cdc11745e5dbabf7f563c8b8875de
soban ejaz <sobanpythonista@gmail.com>
2023-10-04 20:57:02 +0500

    added video link in the code

--- 01. Print Hello World/main.py
+++ 01. Print Hello World/main.py
@@ -1,2 +1,5 @@
 # Wellcome to Programming World and welcome to python programming language
+
+# link to my the tutorial: "https://www.youtube.com/watch?v=8GsGXk6H6dQ&ab_channel=AverageVimmer"
+
 print("Hello World")
