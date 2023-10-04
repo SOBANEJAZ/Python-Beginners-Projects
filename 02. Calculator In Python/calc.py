3352151d3aa3cf4ce5d8090e2230dc2b7c569e6c
soban ejaz <sobanpythonista@gmail.com>
2023-10-04 20:49:17 +0500

    Update calc.py

--- 02. Calculator In Python/calc.py
+++ 02. Calculator In Python/calc.py
@@ -1,10 +1,11 @@
 # BAsic calculator in python
 while True:
 
+
+    operator = input("Enter operator(+ - * /): \n \t ")
     operator = input("Enter operator(+ - * /): \n \t ")
     num1 = float(input("Enter your first number:"))
     num2 = float(input("Enter your second number:"))
-
     if operator == "+":
         result = num1 + num2
         print(result)
